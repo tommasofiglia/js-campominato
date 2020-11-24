cf30f9c3d74bad9227ab7fa181d93969e2d5c47f
Tommaso Figlia <tommasofiglia@gmail.com>
2020-11-24 13:00:03 +0100

    Revisione finale
    Ho sostituito "includes" con una funzione creata ad hoc per vedere se un numero appartiene ad un array.

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,10 +19,25 @@ switch (modality) {
 }
 
 /*********** SPIEGAZIONE DI QUANTO SEGUE **************
-Sotto ho creato due funzioni diverse. La prima gestisce la creazione dei 16 numeri randomici per il pc, la seconda l'inserimento del numero da parte dell'utente e il confronto di esso con i numeri presenti nell'array dei numeri generati casualmente dal pc.
-Ho diviso questo codice in due funzioni diverse anche se avrei potuto crearne una sola avente "maxnumber" come argomento. Ho fatto ciò per una questione di ordine nel mio ragionamento personale e per tenermi allenato con le funzioni.
+
+Sotto ho creato tre funzioni diverse.
+La prima è la funzione "verifyInArray", in grado di verificare se un dato numero sia presente o meno in un array. Questa serve per riconoscere la presenza del numero inserito dall'utente nell'array dei numeri già scelti (evitando che l'utente scelga due volte lo stesso numero) e poi per evitare che ci siano dei numeri che si ripetono nell'array di 16 numeri Randomici generati dal pc.
+La seconda funzione invece gestisce la creazione dei 16 numeri randomici per il pc, mentre la terza l'inserimento del numero da parte dell'utente.
+Ho diviso questa parte di codice js in due funzioni diverse anche se avrei potuto crearne una sola avente "maxnumber" come argomento. Ho fatto ciò per una questione di ordine nel mio ragionamento personale e per tenermi allenato con le funzioni.
+
 ****************************************************/
 
+//Creo la funzione che verifichi la presenza di un numero in un determinato array.
+
+function verifyInArray(array, number) {
+   var i = 0;
+   while ( i < array.length) {
+     if (number === array [i]) {
+       return true;
+     }
+     i++;
+   }
+}
 
 //Creo una funzione che generi 16 numeri casuali che vadano da 1 al numero scelto dall'utente (in base alla difficoltà) per il pc ( sono le "bombe" che fanno perdere l' utente)
 
@@ -36,11 +51,9 @@ function randomPcNumbers(maxnumber) {
   while (pcNumbers.length < 16) {
     var randomNumber = Math.floor(Math.random() * maxnumber) + 1;
 
-    if (pcNumbers.includes(randomNumber)) {
-    } else {
+    if (! verifyInArray(pcNumbers, randomNumber)) { //uso di "verifyInArray"
       pcNumbers.push(randomNumber);
     }
-
     i++
   }
 }
@@ -58,7 +71,7 @@ function gameCampoMinato(maxnumber) {
   while (userNumbers.length < (maxnumber - 16)) {
     var insertNumber = Number(prompt("Inserisci un numero da 1 a " + maxnumber));
 
-    if (userNumbers.includes(insertNumber)) {
+    if (verifyInArray(userNumbers, insertNumber)) { //uso di "verifyInArray"
       alert("Numero già inserito in precedenza");
     } else if ((insertNumber > maxnumber) || (insertNumber === 0)) {
       alert("Devi inserire un numero compreso tra 1 e " + maxnumber + "!");
@@ -70,10 +83,10 @@ function gameCampoMinato(maxnumber) {
       // Creo una if che imposti la condizione per andare avanti, perdere o vincere il gioco quando il numero inserito dall'utente è valido (Cioé se non si ripete)
 
       if (pcNumbers.includes(insertNumber)) {
-        alert("Hai perso!!! " + "Il tuo punteggio è " + (userNumbers.length - 1));
+        alert("Hai perso!!! " + "Il tuo punteggio è di " + (userNumbers.length - 1) + " numeri inseriti prima di trovare la bomba!");
         location.reload();
       } else if (userNumbers.length == (maxnumber - 16)) {
-        alert("Complimenti! Hai vinto il gioco");
+        alert("Complimenti! Hai vinto il gioco con un punteggio di " + (userNumbers.length) + " numeri inseriti !!");
         location.reload();
       } else {
         alert("Non hai beccato la bomba, puoi andare avanti");
